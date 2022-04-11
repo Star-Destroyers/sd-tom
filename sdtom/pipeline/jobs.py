519d8f832de0fae524fa9eab8485fbb1df6ecf82
Austin Riba <austin@m51.io>
2022-04-11 10:35:42 -0700

    black: format jobs.py with black

--- sdtom/pipeline/jobs.py
+++ sdtom/pipeline/jobs.py
@@ -21,30 +21,34 @@ def find_new_tns_classifications():
 
 def update_datums_from_mars(target: Target):
     mars = MARSBroker()
-    alerts = mars.fetch_alerts({'objectId': target.name})
+    alerts = mars.fetch_alerts({"objectId": target.name})
 
     # always get the latest alert
     try:
         alert = next(alerts)
     except StopIteration:
-        logger.info('No alerts for this target')
+        logger.info("No alerts for this target")
         return
 
     mars.process_reduced_data(target, alert)
     try:
-        cache.set(f'latest_mag_{target.id}', target.reduceddatum_set.first().value.get('magnitude'), timeout=60 * 60 * 24 * 30)
+        cache.set(
+            f"latest_mag_{target.id}",
+            target.reduceddatum_set.first().value.get("magnitude"),
+            timeout=60 * 60 * 24 * 30,
+        )
     except Exception:
-        logger.warn('Could not cache latest magnitude.')
+        logger.warn("Could not cache latest magnitude.")
 
 
 def append_queryname(target, query_name):
-    existing_query_name = target.extra_fields.get('query_name')
+    existing_query_name = target.extra_fields.get("query_name")
 
     if existing_query_name:
-        if query_name in existing_query_name.split(','):
+        if query_name in existing_query_name.split(","):
             return existing_query_name
         else:
-            new_query_name = f'{existing_query_name}, {query_name}'
+            new_query_name = f"{existing_query_name}, {query_name}"
     else:
         new_query_name = query_name
 
@@ -56,24 +60,24 @@ def fetch_new_lasair_alerts():
     lasair_broker = LasairIrisBroker()
     for query in queries:
         last_run = query.last_run or timezone.now() - timedelta(days=1)
-        alerts = lasair_broker.fetch_alerts({'since': last_run, **query.parameters})
+        alerts = lasair_broker.fetch_alerts({"since": last_run, **query.parameters})
         while True:
             try:
                 generic_alert = lasair_broker.to_generic_alert(next(alerts))
                 try:
                     target = Target.objects.get(name=generic_alert.name)
-                    logger.info('Updating target ' + str(target))
+                    logger.info("Updating target " + str(target))
                 except Target.DoesNotExist:
                     target, extras, _ = generic_alert.to_target()
                     target.save(extras=extras)
-                    target_list, _ = TargetList.objects.get_or_create(name='New')
+                    target_list, _ = TargetList.objects.get_or_create(name="New")
                     target_list.targets.add(target)
-                    logger.info('Created target ' + str(target))
-                query_name = append_queryname(target, query.parameters['query_name'])
-                target.save(extras={'query_name': query_name})
+                    logger.info("Created target " + str(target))
+                query_name = append_queryname(target, query.parameters["query_name"])
+                target.save(extras={"query_name": query_name})
                 update_datums_from_mars(target)
             except StopIteration:
                 break
-        logger.info('Finished importing new lasair targets')
+        logger.info("Finished importing new lasair targets")
         query.last_run = timezone.now()
         query.save()
