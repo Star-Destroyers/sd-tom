31a85f480fffb3f973bf21c5992583100a815431
Austin Riba <austin@m51.io>
2022-04-11 10:47:14 -0700

    black: format settings.py with black

--- sdtom/settings.py
+++ sdtom/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0rm1glx5go9^k6mtrzai&amp;$lpc$%9&amp;*tq$4(j)fa3p9)mt)#vyc'
+SECRET_KEY = "0rm1glx5go9^k6mtrzai&amp;$lpc$%9&amp;*tq$4(j)fa3p9)mt)#vyc"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -33,117 +33,117 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-TOM_NAME = 'Star Destroyers'
+TOM_NAME = "Star Destroyers"
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django.contrib.sites',
-    'django_extensions',
-    'guardian',
-    'tom_common',
-    'django_comments',
-    'bootstrap4',
-    'crispy_forms',
-    'rest_framework',
-    'rest_framework.authtoken',
-    'django_filters',
-    'django_gravatar',
-    'tom_targets',
-    'tom_alerts',
-    'tom_catalogs',
-    'tom_observations',
-    'tom_dataproducts',
-    'sdtom.pipeline'
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "django.contrib.sites",
+    "django_extensions",
+    "guardian",
+    "tom_common",
+    "django_comments",
+    "bootstrap4",
+    "crispy_forms",
+    "rest_framework",
+    "rest_framework.authtoken",
+    "django_filters",
+    "django_gravatar",
+    "tom_targets",
+    "tom_alerts",
+    "tom_catalogs",
+    "tom_observations",
+    "tom_dataproducts",
+    "sdtom.pipeline",
 ]
 
 SITE_ID = 1
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'tom_common.middleware.Raise403Middleware',
-    'tom_common.middleware.ExternalServiceMiddleware',
-    'tom_common.middleware.AuthStrategyMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "tom_common.middleware.Raise403Middleware",
+    "tom_common.middleware.ExternalServiceMiddleware",
+    "tom_common.middleware.AuthStrategyMiddleware",
 ]
 
-ROOT_URLCONF = 'sdtom.urls'
+ROOT_URLCONF = "sdtom.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
+CRISPY_TEMPLATE_PACK = "bootstrap4"
 
-WSGI_APPLICATION = 'sdtom.wsgi.application'
+WSGI_APPLICATION = "sdtom.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
 
-DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
-LOGIN_URL = '/accounts/login/'
-LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/'
+LOGIN_URL = "/accounts/login/"
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
 
 AUTHENTICATION_BACKENDS = (
-    'django.contrib.auth.backends.ModelBackend',
-    'guardian.backends.ObjectPermissionBackend',
+    "django.contrib.auth.backends.ModelBackend",
+    "guardian.backends.ObjectPermissionBackend",
 )
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -151,74 +151,69 @@ USE_L10N = False
 
 USE_TZ = True
 
-DATETIME_FORMAT = 'Y-m-d H:m:s'
-DATE_FORMAT = 'Y-m-d'
+DATETIME_FORMAT = "Y-m-d H:m:s"
+DATE_FORMAT = "Y-m-d"
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '_static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-MEDIA_ROOT = os.path.join(BASE_DIR, 'data')
-MEDIA_URL = '/data/'
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "_static")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+MEDIA_ROOT = os.path.join(BASE_DIR, "data")
+MEDIA_URL = "/data/"
 
 LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
         }
     },
-    'loggers': {
-        '': {
-            'handlers': ['console'],
-            'level': 'INFO'
-        }
-    }
+    "loggers": {"": {"handlers": ["console"], "level": "INFO"}},
 }
 
 # Caching
 # https://docs.djangoproject.com/en/dev/topics/cache/#filesystem-caching
 
 CACHES = {
-    'default': {
-        'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': os.getenv('REDIS_URL', 'redis://127.0.0.1:6379/1'),
-        'OPTIONS': {
-            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
-        }
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": os.getenv("REDIS_URL", "redis://127.0.0.1:6379/1"),
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        },
     }
 }
 
 # TOM Specific configuration
-TARGET_TYPE = 'SIDEREAL'
+TARGET_TYPE = "SIDEREAL"
 
 FACILITIES = {
-    'LCO': {
-        'portal_url': 'https://observe.lco.global',
-        'api_key': os.getenv('LCO_API_KEY', ''),
+    "LCO": {
+        "portal_url": "https://observe.lco.global",
+        "api_key": os.getenv("LCO_API_KEY", ""),
     },
-    'GEM': {
-        'portal_url': {
-            'GS': 'https://139.229.34.15:8443',
-            'GN': 'https://128.171.88.221:8443',
+    "GEM": {
+        "portal_url": {
+            "GS": "https://139.229.34.15:8443",
+            "GN": "https://128.171.88.221:8443",
         },
-        'api_key': {
-            'GS': '',
-            'GN': '',
+        "api_key": {
+            "GS": "",
+            "GN": "",
         },
-        'user_email': '',
-        'programs': {
-            'GS-YYYYS-T-NNN': {
-                'MM': 'Std: Some descriptive text',
-                'NN': 'Rap: Some descriptive text'
+        "user_email": "",
+        "programs": {
+            "GS-YYYYS-T-NNN": {
+                "MM": "Std: Some descriptive text",
+                "NN": "Rap: Some descriptive text",
             },
-            'GN-YYYYS-T-NNN': {
-                'QQ': 'Std: Some descriptive text',
-                'PP': 'Rap: Some descriptive text',
+            "GN-YYYYS-T-NNN": {
+                "QQ": "Std: Some descriptive text",
+                "PP": "Rap: Some descriptive text",
             },
         },
     },
@@ -227,60 +222,54 @@ FACILITIES = {
 # Define the valid data product types for your TOM. Be careful when removing items, as previously valid types will no
 # longer be valid, and may cause issues unless the offending records are modified.
 DATA_PRODUCT_TYPES = {
-    'photometry': ('photometry', 'Photometry'),
-    'fits_file': ('fits_file', 'FITS File'),
-    'spectroscopy': ('spectroscopy', 'Spectroscopy'),
-    'image_file': ('image_file', 'Image File')
+    "photometry": ("photometry", "Photometry"),
+    "fits_file": ("fits_file", "FITS File"),
+    "spectroscopy": ("spectroscopy", "Spectroscopy"),
+    "image_file": ("image_file", "Image File"),
 }
 
 DATA_PROCESSORS = {
-    'photometry': 'tom_dataproducts.processors.photometry_processor.PhotometryProcessor',
-    'spectroscopy': 'tom_dataproducts.processors.spectroscopy_processor.SpectroscopyProcessor',
+    "photometry": "tom_dataproducts.processors.photometry_processor.PhotometryProcessor",
+    "spectroscopy": "tom_dataproducts.processors.spectroscopy_processor.SpectroscopyProcessor",
 }
 
 TOM_FACILITY_CLASSES = [
-    'tom_observations.facilities.lco.LCOFacility',
-    'tom_observations.facilities.gemini.GEMFacility',
-    'tom_observations.facilities.soar.SOARFacility',
-    'tom_observations.facilities.lt.LTFacility'
+    "tom_observations.facilities.lco.LCOFacility",
+    "tom_observations.facilities.gemini.GEMFacility",
+    "tom_observations.facilities.soar.SOARFacility",
+    "tom_observations.facilities.lt.LTFacility",
 ]
 
 TOM_ALERT_CLASSES = [
-    'tom_alerts.brokers.alerce.ALeRCEBroker',
-    'tom_alerts.brokers.antares.ANTARESBroker',
-    'tom_alerts.brokers.gaia.GaiaBroker',
-    'tom_alerts.brokers.lasair.LasairBroker',
-    'tom_alerts.brokers.mars.MARSBroker',
-    'tom_alerts.brokers.scimma.SCIMMABroker',
-    'tom_alerts.brokers.scout.ScoutBroker',
-    'tom_alerts.brokers.tns.TNSBroker',
-    'tom_alerts.brokers.fink.FinkBroker',
-    'sdtom.alerts.lasair_iris.LasairIrisBroker'
+    "tom_alerts.brokers.alerce.ALeRCEBroker",
+    "tom_alerts.brokers.antares.ANTARESBroker",
+    "tom_alerts.brokers.gaia.GaiaBroker",
+    "tom_alerts.brokers.lasair.LasairBroker",
+    "tom_alerts.brokers.mars.MARSBroker",
+    "tom_alerts.brokers.scimma.SCIMMABroker",
+    "tom_alerts.brokers.scout.ScoutBroker",
+    "tom_alerts.brokers.tns.TNSBroker",
+    "tom_alerts.brokers.fink.FinkBroker",
+    "sdtom.alerts.lasair_iris.LasairIrisBroker",
 ]
 
 BROKERS = {
-    'TNS': {
-        'api_key': os.getenv('TNS_API_KEY'),
-        'bot_id': os.getenv('TNS_BOT_ID'),
-        'bot_name': os.getenv('TNS_BOT_NAME')
+    "TNS": {
+        "api_key": os.getenv("TNS_API_KEY"),
+        "bot_id": os.getenv("TNS_BOT_ID"),
+        "bot_name": os.getenv("TNS_BOT_NAME"),
     },
-    'LASAIRIRIS': {
-        'api_key': os.getenv('LASAIR_API_KEY')
-    }
+    "LASAIRIRIS": {"api_key": os.getenv("LASAIR_API_KEY")},
 }
 
 TOM_HARVESTER_CLASSES = [
-    'tom_catalogs.harvesters.simbad.SimbadHarvester',
-    'tom_catalogs.harvesters.ned.NEDHarvester',
-    'tom_catalogs.harvesters.jplhorizons.JPLHorizonsHarvester',
-    'tom_catalogs.harvesters.tns.TNSHarvester',
+    "tom_catalogs.harvesters.simbad.SimbadHarvester",
+    "tom_catalogs.harvesters.ned.NEDHarvester",
+    "tom_catalogs.harvesters.jplhorizons.JPLHorizonsHarvester",
+    "tom_catalogs.harvesters.tns.TNSHarvester",
 ]
 
-HARVESTERS = {
-    'TNS': {
-        'api_key': ''
-    }
-}
+HARVESTERS = {"TNS": {"api_key": ""}}
 
 # Define extra target fields here. Types can be any of "number", "string", "boolean" or "datetime"
 # See https://tomtoolkit.github.io/docs/target_fields for documentation on this feature
@@ -292,13 +281,13 @@ HARVESTERS = {
 #     {'name': 'dicovery_date', 'type': 'datetime'}
 # ]
 EXTRA_FIELDS = [
-    {'name': 'classification', 'type': 'string'},
-    {'name': 'query_name', 'type': 'string'}
+    {"name": "classification", "type": "string"},
+    {"name": "query_name", "type": "string"},
 ]
 
 # Authentication strategy can either be LOCKED (required login for all views)
 # or READ_ONLY (read only access to views)
-AUTH_STRATEGY = 'READ_ONLY'
+AUTH_STRATEGY = "READ_ONLY"
 
 # Row-level data permissions restrict users from viewing certain objects unless they are a member of the group to which
 # the object belongs. Setting this value to True will allow all `ObservationRecord`, `DataProduct`, and `ReducedDatum`
@@ -311,11 +300,11 @@ TARGET_PERMISSIONS_ONLY = True
 OPEN_URLS = []
 
 HOOKS = {
-    'target_post_save': 'tom_common.hooks.target_post_save',
-    'observation_change_state': 'tom_common.hooks.observation_change_state',
-    'data_product_post_upload': 'tom_dataproducts.hooks.data_product_post_upload',
-    'data_product_post_save': 'tom_dataproducts.hooks.data_product_post_save',
-    'multiple_data_products_post_save': 'tom_dataproducts.hooks.multiple_data_products_post_save',
+    "target_post_save": "tom_common.hooks.target_post_save",
+    "observation_change_state": "tom_common.hooks.observation_change_state",
+    "data_product_post_upload": "tom_dataproducts.hooks.data_product_post_upload",
+    "data_product_post_save": "tom_dataproducts.hooks.data_product_post_save",
+    "multiple_data_products_post_save": "tom_dataproducts.hooks.multiple_data_products_post_save",
 }
 
 AUTO_THUMBNAILS = False
@@ -328,16 +317,15 @@ HINTS_ENABLED = True
 HINT_LEVEL = 20
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-    ],
-    'TEST_REQUEST_DEFAULT_FORMAT': 'json',
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 100
+    "DEFAULT_PERMISSION_CLASSES": [],
+    "TEST_REQUEST_DEFAULT_FORMAT": "json",
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
+    "PAGE_SIZE": 100,
 }
 
 django_heroku.settings(locals())
 
 try:
-    from local_settings import * # noqa
+    from local_settings import *  # noqa
 except ImportError:
     pass
