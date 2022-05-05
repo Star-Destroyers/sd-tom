98498f822d0e1f100f066110a0a597d664705101
Austin Riba <austin@m51.io>
2022-05-05 09:20:56 -0700

    Use built in redis cache

--- sdtom/settings.py
+++ sdtom/settings.py
@@ -180,11 +180,8 @@ LOGGING = {
 
 CACHES = {
     "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
         "LOCATION": os.getenv("REDIS_URL", "redis://127.0.0.1:6379/1"),
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-        },
     }
 }
 
